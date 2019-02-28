533742b8afb1f303abd9b33b49450f0d1e82b42e
unknown <mikey@un-loop.org>
2019-02-27 20:20:17 -0800

    update front end every 3 sec

--- public/index.js
+++ public/index.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
+    setInterval(getWeather, 3000);
+});
+
+function getWeather() {
     axios
         .get('/all')
         .then(function(response) {
             console.log(response.data);
+            $('#data').text('');
             $('#data').text(
                 response.data.rows
                     .map(obj => `${obj.time_recorded}, ${obj.pressure}, ${obj.humidity}, ${obj.temp_from_pressure}, ${obj.temp_from_humidity}`)
                     .join('\n')
             );
         })
-});
+}
