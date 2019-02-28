b6bf648773d5e129c2f76f852e29d8163956a936
unknown <mikey@un-loop.org>
2019-02-28 14:25:21 -0800

    no text

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post('/record', (req, res) => {
             });
         });
 
-        checkTemp(req.body.temp_from_humidity);
+        // checkTemp(req.body.temp_from_humidity);
 });
 
 function checkTemp(temp) {
