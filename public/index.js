fc7a2f69fc11b7db76fdcb0120089cddaeee5684
unknown <mikey@un-loop.org>
2019-02-28 13:58:21 -0800

    do not text repeatedly

--- public/index.js
+++ public/index.js
@@ -7,11 +7,27 @@ function getWeather() {
         .get('/all')
         .then(function(response) {
             console.log(response.data);
-            $('#data').text('');
-            $('#data').text(
-                response.data.rows
-                    .map(obj => `${obj.time_recorded}, ${obj.pressure}, ${obj.humidity}, ${obj.temp_from_pressure}, ${obj.temp_from_humidity}`)
-                    .join('\n')
+
+            var temps = response.data.rows;
+            var rows = temps.map(
+                d => $('<tr>')
+                    .append($('<td>').text(d.time_recorded))
+                    .append($('<td>').text(d.pressure))
+                    .append($('<td>').text(`${d.humidity}%`))
+                    .append($('<td>').text(d.temp_from_pressure))
+                    .append($('<td>').text(d.temp_from_humidity))
             );
+
+            $('#data tbody')
+                .text('')
+                .append(rows);
+
+
+            // $('#data').text('');
+            // $('#data').text(
+            //     response.data.rows
+            //         .map(obj => `${obj.time_recorded}, ${obj.pressure}, ${obj.humidity}, ${obj.temp_from_pressure}, ${obj.temp_from_humidity}`)
+            //         .join('\n')
+            // );
         })
 }
--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const path = require('path');
 
 const port = 8080;
 const app = express();
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database(path.join(__dirname, 'data', 'weather.sqlt'));
 const AUTUMN_PHONE_NUMBER = '2533022119';
+const TEMPERATURE_BOUNDS = { min: 1.67, max: 26.6 } // 35F to 80F
 
 if (!process.argv[2]) {
     console.log('provide textbelt key');
@@ -66,26 +67,51 @@ app.post('/record', (req, res) => {
             });
         });
 
-        // checkTemp(req.body.temp_from_humidity);
+        checkTemp(req.body.temp_from_humidity);
 });
 
 function checkTemp(temp) {
-    if (temp < 0) {
-        request.post('https://textbelt.com/text', {
-            form: {
-                phone: AUTUMN_PHONE_NUMBER,
-                message: `The temperature has reached ${temp}.`,
-                key: TEXTBELT_KEY,
-            },
-        },
-        function (err, httpResponse, body) {
-            if (err) {
-                console.error('Error:', err);
-                return;
-            }
-            console.log(JSON.parse(body));
-        })
+    if (temp >= TEMPERATURE_BOUNDS.min && temp <= TEMPERATURE_BOUNDS.max) {
+        console.log('no alert');
+        return
     }
+
+    
+    db.get(`Select Cast ((
+        JulianDay('now') - JulianDay(time_sent)
+    ) * 24 As REAL) as elapsed FROM Alert ORDER BY elapsed ASC LIMIT 1`, (err, row) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(row);
+            if (!row || row.elapsed > 1) { // if have not sent a text in the last hour
+                sendText();
+            } else {
+                console.log('not texting -- already sent one this hour')
+            }
+        }
+    })
+
+
+}
+
+function sendText(temp) {
+    console.log(`!!! SENDING TEXT!!! The temperature has reached ${temp}.`);
+    db.run('INSERT INTO Alert (phone_no) VALUES (?)', AUTUMN_PHONE_NUMBER);
+    request.post('https://textbelt.com/text', {
+        form: {
+            phone: AUTUMN_PHONE_NUMBER,
+            message: `The temperature has reached ${temp}.`,
+            key: TEXTBELT_KEY,
+        },
+    },
+    function (err, httpResponse, body) {
+        if (err) {
+            console.error('Error:', err);
+            return;
+        }
+        console.log(JSON.parse(body));
+    })
 }
 
 function verifyData(data) {
@@ -99,7 +125,7 @@ function verifyData(data) {
 
 
 function initializeDb() {
-    var createTable = `CREATE TABLE IF NOT EXISTS Environment (
+    var createEnvTable = `CREATE TABLE IF NOT EXISTS Environment (
         time_recorded TEXT DEFAULT CURRENT_TIMESTAMP,
         humidity REAL,
         pressure REAL,
@@ -107,12 +133,23 @@ function initializeDb() {
         temp_from_humidity REAL
     );`;
 
-    db.run(createTable, err => {
+    var createAlertTable = `CREATE TABLE IF NOT EXISTS Alert (
+        time_sent TEXT DEFAULT CURRENT_TIMESTAMP,
+        phone_no TEXT NOT NULL
+    )`;
+
+    const checkErr = err => {
         if (err)
             console.log(err);
         else
             startServer();
-    });
+    };
+
+    db.serialize(() => {
+        db.run(createEnvTable);
+        db.run(createAlertTable, checkErr);
+    })
+
 }
 
 function startServer() {
